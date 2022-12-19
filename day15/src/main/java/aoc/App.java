623df2cc33ee81edfe6f4844573b8bff52c97ff0
Benjamin Eriksen <benja.e95@gmail.com>
2022-12-19 15:02:31 +0100

    day15 2/2

--- day15/src/main/java/aoc/App.java
+++ day15/src/main/java/aoc/App.java
@@ -4,55 +4,56 @@ import java.awt.Point;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 
 public class App {
 
-
-
     public static Integer getSolutionPart1(List<String> lines, HashMap<Point, Character> map) {
-        HashMap<Point, Integer> sensors = new HashMap<>();
-        lines.stream().map(l-> new Scanner(l).useDelimiter("[^0-9]+")).forEach(s-> {
-            var x1 = s.nextInt();
-            var y1 = s.nextInt();
-            var x2 = s.nextInt();
-            var y2 = s.nextInt();
-
-            sensors.put(new Point(x1,y1), Math.abs(x1 - x2) + Math.abs(y1 - y2));
-        });
+        HashMap<Point, Integer> sensors = deriveSensorRanges(lines);
         int count = 0;
-        var s = "";
-        for (int x = -1000000; x <= 1000000; x++) {
-            var p = new Point(x,10);
-            if (map.containsKey(p) && map.get(p) == 'B') {
-                s+= "B";
-                continue;
-            }
-            if (inRange(p, sensors)) {
-                count++;
-                s+= "#";
-                //System.out.println(x + " " + 10 + " with " + map.get(new Point(x,10)));
-                //System.out.println(inRange(x,10, sensors) + " c" + count);
-            } else {
-                System.out.println("not");
-                s+= ".";
-            }
-        }
+        for (int x = -3000000; x <= 10000000; x++) {
+            var p = new Point(x, 2000000);
+            if (map.containsKey(p) && map.get(p) == 'B') continue;
+            if (inRange(p, sensors))  count++;
+        } return count;
+    }
 
-        System.out.println();
-        System.out.println(s.replace("####################################################################################################", "!"));
-        return count;
+    private static HashMap<Point, Integer> deriveSensorRanges(List<String> lines) {
+        var sensors = new HashMap<Point, Integer>();
+        lines.stream().map(l-> new Scanner(l).useDelimiter("[^-0-9]+")).forEach(s-> {
+            int x1 = s.nextInt(), y1 = s.nextInt(), x2 = s.nextInt(), y2 = s.nextInt();
+            sensors.put(new Point(x1,y1), Math.abs(x1 - x2) + Math.abs(y1 - y2));
+        }); return sensors;
+    }
+
+    private static int distance(Point current, HashMap<Point, Integer> sensorRanges) {
+        var sensorInRange = sensorRanges.keySet().stream().filter(s -> (Math.abs(s.x - current.x) + Math.abs(s.y - current.y)) <= sensorRanges.get(s)).findAny();
+        if (sensorInRange.isPresent()) {
+            var sensor = sensorInRange.get();
+            return sensorRanges.get(sensor) - (Math.abs(sensor.x - current.x) + Math.abs(sensor.y - current.y) );
+        } else return -1;
     }
 
     private static boolean inRange(Point p, HashMap<Point, Integer> sensors) {
         return sensors.keySet().stream().anyMatch(s -> (Math.abs(s.x - p.x) + Math.abs(s.y - p.y)) <= sensors.get(s));
     }
 
-    public static Integer getSolutionPart2(List<String> input) {
-        return 0;
+    public static Long getSolutionPart2(List<String> lines, HashMap<Point, Character> map) {
+        HashMap<Point, Integer> sensors = deriveSensorRanges(lines);
+        int from = 0, to = 4000000;
+        for (int y = from; y < to; y++) {
+            for (int x = from; x < to; x++) {
+                var c = new Point(x,y);
+                long d = distance(c, sensors);
+                if (d == -1) {
+                    return x*4000000L + y;
+                } else {
+                    if (d > 0) x+= d;
+                }
+            }
+        } return -1L;
     }
 
     public static void main(String[] args) throws IOException {
@@ -61,7 +62,7 @@ public class App {
         var input = parseInput("input.txt");
         var map = toMap(input);
         if (part.equals("part2")) {
-            System.out.println(getSolutionPart2(input));
+            System.out.println(getSolutionPart2(input, map));
         }
         else
             System.out.println(getSolutionPart1(input, map));
@@ -70,30 +71,11 @@ public class App {
     private static List<String> parseInput(String filename) throws IOException {
         return Files.lines(Paths.get(filename)).toList();
     }
-
-    private static void printMap(HashMap<Point, Character> map) {
-        for (int y = -2; y <= 22; y++) {
-            String s = "";
-            for (int x = -2; x <= 25; x++) {
-                if (map.containsKey(new Point(x, y))) {
-                    s += map.get(new Point(x, y));
-                }
-                else {
-                    s += ".";
-                }
-            }
-            System.out.println(s);
-        }
-    }
-
     private static HashMap<Point, Character> toMap(List<String> lines) {
-
         HashMap<Point, Character> map = new HashMap<>();
         lines.stream().map(l-> new Scanner(l).useDelimiter("[^0-9]+")).forEach(s-> {
             map.put(new Point(s.nextInt(),s.nextInt()), 'S');
             map.put(new Point(s.nextInt(),s.nextInt()), 'B');
-        });
-        return map;
+        }); return map;
     }
-
 }
